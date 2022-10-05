6d33ee1dcb2e41de891aaa666d378e09144237cd
tahahasan7 <taha.hasan@student.ehb.be>
2022-10-05 16:30:22 +0200

    change

--- exercises/1-functions/1-0-name.js
+++ exercises/1-functions/1-0-name.js
@@ -1,1 +1,47 @@
 "use strict";
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+drawName();
+
+function drawName() {
+
+ context.strokeStyle ="red";
+ context.lineWidth = 5;
+ context.beginPath();
+ context.moveTo(300,200);
+ context.lineTo(300,450);
+ context.moveTo(150,200);
+ context.lineTo(450,200)
+ context.stroke();
+
+
+
+ context.strokeStyle = "blue";
+ context.lineWidth = 5;
+ context.beginPath()
+ context.moveTo(650,200)
+ context.lineTo(550,450)
+ context.moveTo(650,200)
+ context.lineTo(750,450)
+ context.moveTo(590,350)
+ context.lineTo(710,350)
+ context.stroke()
+
+ context.strokeStyle = "green";
+ context.lineWidth = 5;
+ context.beginPath()
+ context.moveTo(850,200)
+ context.lineTo(850,450)
+ context.moveTo(1000,200)
+ context.lineTo(1000,450)
+ context.moveTo(590,350)
+
+ 
+ context.stroke()
+
+
+ }
