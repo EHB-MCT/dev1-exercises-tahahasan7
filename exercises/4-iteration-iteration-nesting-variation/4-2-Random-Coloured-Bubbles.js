0dfa71b4150c549a8b5852bc4c3739dab1d70bd8
tahahasan7 <taha.hasan@student.ehb.be>
2022-11-01 18:14:43 +0100

    les tot 4

--- exercises/4-iteration-iteration-nesting-variation/4-2-Random-Coloured-Bubbles.js
+++ exercises/4-iteration-iteration-nesting-variation/4-2-Random-Coloured-Bubbles.js
@@ -3,3 +3,22 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
+let margin = 200
+drawBubbles()
+
+
+
+function drawBubbles(){
+
+
+    for(let i=0; i<100; i++)
+    //Utils.drawArc(Math.random() * (innerWidth - margin * 2),Math.random() * (innerHeight - margin * 2), Math.random() *80)
+    //Utils.rgba()
+    let randomX = margin +Math.Random * (widht - margin *2)
+    let randomY= margin +Math.Random() * (height - margin *2)
+    let randomHue= Math.random * 360
+    let randomSize = 10 +Math.Random() * 
+    context.fillstyle
+
+
+}
